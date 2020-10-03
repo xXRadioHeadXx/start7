8a09f82b1ef7c45d9a3fd25bbee0c0f4ab6f4320
GlebZigert <gleb.zigert@yandex.ru>
2020-10-03 05:04:59 +0300

    .

--- m_cfg/mytreeview.cpp
+++ m_cfg/mytreeview.cpp
@@ -1,6 +1,11 @@
 #include "mytreeview.h"
 
+<<<<<<< HEAD
 MyTreeView::MyTreeView()
+=======
+MyTreeView::MyTreeView(QWidget *parent)
+  : QTreeView(parent)
+>>>>>>> 91046e7... .
 {
 
 }
--- m_cfg/mytreeview.h
+++ m_cfg/mytreeview.h
@@ -2,11 +2,20 @@
 #define MYTREEVIEW_H
 
 #include <QObject>
+<<<<<<< HEAD
 
 class MyTreeView
 {
 public:
     MyTreeView();
+=======
+#include <QTreeView>
+
+class MyTreeView : public QTreeView
+{
+public:
+    MyTreeView(QWidget *parent = 0);
+>>>>>>> 91046e7... .
 };
 
 #endif // MYTREEVIEW_H
