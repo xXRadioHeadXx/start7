0c96e004fd75650e4b3a272d6f61a070f43c93f9
GlebZigert <gleb.zigert@yandex.ru>
2021-01-25 14:40:59 +0300

    .

--- m_cfg/ssoiwidget.cpp
+++ m_cfg/ssoiwidget.cpp
@@ -6,6 +6,34 @@ SSOIwidget::SSOIwidget(QWidget *parent) :
     ui(new Ui::SSOIwidget)
 {
     ui->setupUi(this);
+
+
+
+    l_Version.insert(1,Version_1);
+    l_Version.insert(2,Version_2);
+    foreach (QString str, l_Version)
+    {
+    this->ui->Version->addItem(str);
+
+    }
+
+    l_SsoiAutoDkUse.insert(1,SsoiAutoDkUse_0);
+    l_SsoiAutoDkUse.insert(2,SsoiAutoDkUse_1);
+    foreach (QString str, l_SsoiAutoDkUse)
+    {
+    this->ui->SsoiAutoDkUse->addItem(str);
+
+    }
+
+    l_SsoiMOprosVariant.insert(0,SsoiMOprosVariant_0);
+    l_SsoiMOprosVariant.insert(1,SsoiMOprosVariant_1);
+    l_SsoiMOprosVariant.insert(2,SsoiMOprosVariant_2);
+    foreach (QString str, l_SsoiMOprosVariant)
+    {
+    this->ui->SsoiMOprosVariant->addItem(str);
+
+    }
+
 }
 
 SSOIwidget::~SSOIwidget()
@@ -15,5 +43,25 @@ SSOIwidget::~SSOIwidget()
 
 void SSOIwidget::default_options()
 {
+
+}
+
+void SSOIwidget::on_Version_currentIndexChanged(const QString &arg1)
+{
+    if(arg1==Version_1)
+    {
+    this->ui->stackedWidget->setCurrentWidget(this->ui->SSOI);
+    this->ui->SsoiMOprosVariant->setVisible(false);
+    this->ui->lbl_SsoiMOprosVariant->setVisible(false);
+
+
+    }
+    else
+    if(arg1==Version_2)
+    {
+    this->ui->stackedWidget->setCurrentWidget(this->ui->SSOI_M);
+    this->ui->SsoiMOprosVariant->setVisible(true);
+    this->ui->lbl_SsoiMOprosVariant->setVisible(true);
+    }
 
 }
--- m_cfg/ssoiwidget.h
+++ m_cfg/ssoiwidget.h
@@ -2,6 +2,7 @@
 #define SSOIWIDGET_H
 
 #include <QWidget>
+#include <QMap>
 
 namespace Ui {
 class SSOIwidget;
@@ -17,8 +18,26 @@ public:
 
     void default_options();
 
+private slots:
+    void on_Version_currentIndexChanged(const QString &arg1);
+
 private:
     Ui::SSOIwidget *ui;
+
+     QMap <int,QString> l_Version;
+     QString Version_1="ССОИ";
+     QString Version_2="ССОИ-M";
+
+     QMap <int,QString> l_SsoiAutoDkUse;
+     QString SsoiAutoDkUse_0="ВЫКЛ";
+     QString SsoiAutoDkUse_1="ВКЛ";
+
+     QMap <int,QString> l_SsoiMOprosVariant;
+     QString SsoiMOprosVariant_0="ССОИ-М";
+     QString SsoiMOprosVariant_1="Адаптивный опрос - БЛ";
+     QString SsoiMOprosVariant_2="Адаптивный опрос - БЛ2020";
+
+
 };
 
 #endif // SSOIWIDGET_H
