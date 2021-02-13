fdf7e17c1265fe362f05e7f2e3e9d1959589a20d
anton.gavrilin <a.u.gavrilin@gmail.com>
2021-02-13 14:03:58 +0300

    [vol.2]Добавление ДК/АвтоСбросТревог msg0x21 msg0x24 устройств РИФ-РЛМ, РИФ-КРЛ, ТРАССА, Разрыв

--- src/port/DataQueueItem.cpp
+++ src/port/DataQueueItem.cpp
@@ -135,7 +135,7 @@ QByteArray DataQueueItem::makeDK0x21(const UnitNode * un)
            TypeUnitNode::SD_BL_IP == un->getType() ||
            TypeUnitNode::IU_BL_IP == un->getType()) {
             out[1] = static_cast<quint8>(0xFF);
-        } else if(TypeUnitNode::RLM_C == un->getType()) {
+        } else if(TypeUnitNode::RLM_C == un->getType() || TypeUnitNode::RLM_KRL == un->getType()) {
             out[1] = static_cast<quint8>(un->getNum1());
         }
     }
