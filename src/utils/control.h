27e4d7a7dc8832ec7686193d60ac3d10b77bfee8
GlebZigert <gleb.zigert@yandex.ru>
2021-04-01 09:58:54 +0300

    .

---
+++ src/utils/control.h
@@ -0,0 +1,46 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include<UnitNode.h>
+#include <TreeModelUnitNode.h>
+
+
+class Control
+{
+public:
+    Control();
+
+       static bool  pass_to_add(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_GROUP(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_SD_BL_IP(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_IU_BL_IP(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_TG(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_RLM_KRL(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_RLM_C(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_BOD_T4K_M(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_Y4_T4K_M(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_DD_T4K_M(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_BOD_SOTA(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_Y4_SOTA(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_DD_SOTA(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_KL(UnitNode *unit, UnitNode* parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_ONVIF(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_STRAZH_IP(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_NET_DEV(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_SSOI_SD(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_SSOI_IU(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_ADAM(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_TOROS(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_DEVLINE(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_RASTRMTV(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+       static bool  pass_to_add_INFO_TABLO(UnitNode *unit, UnitNode *parrent,TreeModelUnitNode *modelTreeUN);
+
+       static bool correct_UDP_parametres(UnitNode *unit);
+
+       static bool no_equal_unit(TreeModelUnitNode *modelTreeUN,UnitNode *unit,UnitNode *supreme,bool (*is_equal)(UnitNode* unit,UnitNode* un));
+       static bool no_equal_unit_from_one_parent(TreeModelUnitNode *modelTreeUN, UnitNode *unit, UnitNode *parent, bool (*is_equal)(UnitNode *, UnitNode *));
+
+
+};
+
+#endif // CONTROL_H
