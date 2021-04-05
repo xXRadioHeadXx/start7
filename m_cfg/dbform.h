cba9b9e9872ca50a314f83da96e5f7572d1befde
Start7-AstraServer <a.u.gavrilin@gmail.com>
2021-04-05 16:08:35 +0300

    more scripts

--- m_cfg/dbform.h
+++ m_cfg/dbform.h
@@ -62,3 +62,71 @@ signals:
 };
 
 #endif // DBFORM_H
+
+/*
+-- Database: rif_db0
+
+-- DROP DATABASE rif_db0;
+
+CREATE DATABASE rif_db0
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'ru_RU.UTF-8'
+       LC_CTYPE = 'ru_RU.UTF-8'
+       CONNECTION LIMIT = -1;
+
+
+
+ */
+
+/*
+-- Sequence: public.jour_id_seq
+
+-- DROP SEQUENCE public.jour_id_seq;
+
+CREATE SEQUENCE public.jour_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 124646
+  CACHE 1;
+ALTER TABLE public.jour_id_seq
+  OWNER TO root;
+
+ */
+
+/*
+-- Table: public.jour
+
+-- DROP TABLE public.jour;
+
+CREATE TABLE public.jour
+(
+  id integer NOT NULL DEFAULT nextval('jour_id_seq'::regclass),
+  cdate timestamp without time zone NOT NULL DEFAULT now(),
+  mdate timestamp without time zone NOT NULL DEFAULT now(),
+  objectid integer,
+  object character varying(128),
+  comment character varying(256) NOT NULL,
+  reason character varying(256),
+  measures character varying(256),
+  operator character varying(256),
+  operatorid character varying(256),
+  status character varying(32),
+  direction character varying(32),
+  type bigint,
+  flag bigint,
+  d1 bigint,
+  d2 bigint,
+  d3 bigint,
+  d4 bigint,
+  objecttype bigint
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.jour
+  OWNER TO root;
+
+ */
