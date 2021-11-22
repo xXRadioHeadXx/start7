219a6fb2c8248240a59d0b302063034d0a009dd1
GlebZigert <gleb.zigert@yandex.ru>
2021-11-22 11:18:23 +0300

    .

--- m_cfg/unitWidgets/widget_toros.cpp
+++ m_cfg/unitWidgets/widget_toros.cpp
@@ -7,6 +7,24 @@ Widget_TOROS::Widget_TOROS(QWidget *parent, communicationTypeWidget *comm, coord
 {
     ID=TypeUnitNode::TOROS;
     ui->setupUi(this);
+
+    for(int i=1;i<100;i++)
+    {
+     /*
+    QString str;
+
+    str.clear();
+
+    if(i/10<1)
+        str.append("0");
+    //if(i/100<1)
+    //    str.append("0");
+    str.append(QString::number(i));
+    */
+    ui->Num1->addItem(QString::number(i));
+
+
+    }
 }
 
 Widget_TOROS::~Widget_TOROS()
@@ -16,39 +34,77 @@ Widget_TOROS::~Widget_TOROS()
 
 void Widget_TOROS::get_from(UnitNode *unit)
 {
-
+    ui->Num1->setCurrentText(QString::number(unit->getNum1()));
 }
 
 void Widget_TOROS::get_default()
 {
-
+    ui->Num1->setCurrentIndex(0);
 }
 
 
 
 void Widget_TOROS::set_to(UnitNode *unit)
 {
-    unit->setNum1(-1);
-    unit->setNum2(-1);
-    unit->setNum3(-1);
+    unit->setNum1(ui->Num1->currentText().toInt());
 }
 
 void Widget_TOROS::update_name()
 {
-    emit updateName("Группа ");
+    QString Name("Торос-");
+    Name.append(this->ui->Num1->currentText());
+    emit updateName(Name);
 
 }
 
 void Widget_TOROS::setEnabled_option_menu(bool val)
 {
+    ui->Num1->setEnabled(val);
+}
+
+bool Widget_TOROS::accepted(UnitNode *unit)
+{
+    UnitNode* parent;
+    parent = static_cast<UnitNode*>(current->internalPointer()); if(!parent){return false;}
+
+    if((parent->getType()!=TypeUnitNode::GROUP)&&(parent->getType()!=TypeUnitNode::SYSTEM))
+    {
+//        QMessageBox::critical(0,"Ошибка",("может быть добавлен только к группе или к системе"));
+//            dialog.exec();
+        return false;
+
+    }
+
+    return no_equal_unit(unit);
+}
+
+bool Widget_TOROS::equal(UnitNode *un, UnitNode *unit)
+{
+    if(un->getType()==unit->getType())
+    if(un->getNum1()==unit->getNum1())
+        return  true;
+        return false;
 
 }
 
 QString Widget_TOROS::get_string(UnitNode *unit)
 {
     QString string1;
-   //     string1.append("<b>");string1.append(m_TypeUnitNode_d.value(unit->getType()));string1.append(" ");//  Группа</b> ");
-        string1.append("<b>");string1.append(m_TypeUnitNode_d.value(unit->getType()));string1.append("</b> ");//  ");string1.append(m_TypeUnitNode_d.value(unit->getType()));string1.append("</b>");//
-    string1.append(unit->getName());
+
+    string1.append("Торос\n");
+    string1.append("Трасса: ");
+    string1.append(QString::number(unit->getNum1()));
+    string1.append("\n");
+    string1.append("Канал: ");
+    string1.append(QString::number(unit->getUdpPort()));
+    string1.append("\n");
+    string1.append("Таймаут: ");
+    string1.append(QString::number(unit->getUdpTimeout()));
+    string1.append("\n");
    return string1;
+}
+
+void Widget_TOROS::on_Num1_currentIndexChanged(const QString &arg1)
+{
+    update_name();
 }
